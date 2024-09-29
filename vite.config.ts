a664b0c32f0c787507339522b51ae409305a3861
Tan Sia How <tansiahow1996@gmail.com>
2024-09-29 14:58:04 +0800

    refactor: install unplugin-vue-router

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+import VueRouter from 'unplugin-vue-router/vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/money-log/',
   plugins: [
+    VueRouter({
+      /* options */
+    }),
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
